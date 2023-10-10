0469c9e58f80c6ee2a2a0b7f3fb97410d57f9a32
RegisLewis <regisaslewis@gmail.com>
2023-10-09 20:42:37 -0700

    email parser lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,10 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        pattern = r"[A-z0-9\.]+@[A-z]+\.[a-z]{3,}"
+        regex = re.compile(pattern)
+        return sorted(set(regex.findall(self.email_addresses)))
